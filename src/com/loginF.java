2779af3d97d5967097c1f49e8ae9c6b732a606ea
hopeanddespair114 <Jincheng.19@intl.zju.edu.cn>
2021-05-11 16:24:35 +0800

    db

--- src/com/loginF.java
+++ src/com/loginF.java
@@ -9,18 +9,30 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class loginF extends OncePerRequestFilter{
-    private String n;
-    private String p;
+    private String n = "王健强";
+    private String p = "zhenshen";
+    private String name = null;
+    private String key = null;
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        n = "王健强";
-        p = "zhenshen";
-        String name = httpServletRequest.getParameter("name");
-        String key = httpServletRequest.getParameter("key");
+        if (httpServletRequest.getServletPath().equals("/register.do")) {
+            n = httpServletRequest.getParameter("name");
+            p = httpServletRequest.getParameter("key");
+        }
+        if (name == null) {
+            name = httpServletRequest.getParameter("name");
+        }
+        if (key == null){
+            key = httpServletRequest.getParameter("key");
+        }
         if (name.equals(n) && key.equals(p)){
+            httpServletRequest.setAttribute("name",name);
+            httpServletRequest.setAttribute("key",key);
             filterChain.doFilter(httpServletRequest, httpServletResponse);
         }
         else{
+            name = null;
+            key = null;
             httpServletResponse.sendRedirect("error.do");
         }
     }
--- web/drug.jsp
+++ web/drug.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"  %>
 <html>
 <head>
     <link rel="stylesheet" href="static/css/style.css" type="text/css"/>
@@ -34,6 +35,29 @@
     <div class="page-headline">Drugs</div>
     <div id="main">
         <div id="content">
+            <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Drug Url</th>
+                        <th>Biomarker</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <c:forEach items="${drugs}" var="item">
+                        <tr>
+                            <td>${item.id}</td>
+                            <td>${item.name}</td>
+                            <td>${item.drugUrl}</td>
+                            <td>${item.biomarker}</td>
+                        </tr>
+                    </c:forEach>
+
+                    </tbody>
+                </table>
+            </div>
             <div class="post" >
                 <h3 class="post-title"><a href="#">${message}</a></h3>
             </div>
